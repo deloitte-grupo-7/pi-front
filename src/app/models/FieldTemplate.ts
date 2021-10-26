1a9dda707a2b21f28b4fecfa39b1fa2b4d23104b
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-10-26 11:08:56 -0300

    requisição http de cadastro

---
+++ src/app/models/FieldTemplate.ts
@@ -0,0 +1,6 @@
+export interface FieldTemplate {
+    title: string,
+    name: string,
+    type: string,
+    regex?: RegExp,
+  }
--- src/app/services/signup-validation.service.ts
+++ src/app/services/signup-validation.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FieldTemplate } from '../models/FieldTemplate';
+import { SignUpForm } from '../models/SignUpForm';
+
+const fields: FieldTemplate[] = [
+  {
+    title: 'Nome de usuário', 
+    name: 'username',
+    type: 'text',
+    regex: /[a-zA-Z\d]+/,
+  },
+  {
+    title: 'Nome completo',
+    name: 'name',
+    type: 'text',
+    regex: /[a-zA-ZÀ-ú\s]+/,
+  },
+  {
+    title: 'CPF',
+    name: 'cpf',
+    type: 'text',
+    regex: /\d+/,
+  },
+  {
+    title: 'Email',
+    name: 'email',
+    type: 'email',
+    regex: /^[^\_\.\-][\w\d\.\-]{4,}(?<![\.\_\-])\@\w{2,}(\.{1}[a-zA-Z]{2,})(?!\.)$/,
+  },
+  {
+    title: 'Data de nascimento',
+    name: 'birthday',
+    type: 'date',
+  },
+  {
+    title: 'Senha',
+    name: 'password',
+    type: 'password',
+  },
+  {
+    title: 'Confirmação de senha',
+    name: 'passconf',
+    type: 'password',
+  }];
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpValidationService {
+  fields: FieldTemplate[] = fields;
+  private readonly apiURL: string;
 
   config = {
     required: 'Obrigatório',
@@ -12,9 +60,17 @@ export class SignUpValidationService {
     invalidCpf: 'CPF inválido',
     invalidEmail: 'Email inválido',
     invalidPassword: 'A senha deve ter pelo menos 8 caracteres e incluir uma letra maiúcula, uma minúscula, um número, e um caractere especial.',
+  }
 
+  constructor(private http:HttpClient){
+    this.apiURL = 'http://localhost:8080';
   }
 
   static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
   }
+
+  postRequest(form: SignUpForm) {
+    return this.http.post(`${this.apiURL}/register`, form)
+  }
+
 }
