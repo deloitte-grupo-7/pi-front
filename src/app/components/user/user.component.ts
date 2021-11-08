d11d19c3243e2257195595caff2697d1e0b9f529
midorishibukawa <shibukawa.midori@gmail.com>
2021-11-08 06:53:57 -0300

    ajuste de margem do header

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/Classes';
 import { HttpService } from 'src/app/services/http.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user',
@@ -12,7 +13,7 @@ export class UserComponent implements OnInit {
 
   user!: User;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
     const username: string = this.router.url.substring(3);
 
     HttpService.getUser(username).subscribe(
@@ -21,7 +22,7 @@ export class UserComponent implements OnInit {
           console.log(data);
           this.user = data;
         },
-        error: (err: Error) => console.error(err)
+        // error: (err: Error) => this.router.navigateByUrl('404')
       }
     );
   }
@@ -33,7 +34,6 @@ export class UserComponent implements OnInit {
     return `foto de ${this.user.name}`;
   }
   getImgUrl(): string {
-    const imgUrl: string = this.user.imgUrl;
-    return imgUrl ? imgUrl : 'https://images.unsplash.com/photo-1636306950045-4dbb10b7e0f4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80';
+    return this.user.imgUrl ? this.user.imgUrl : AuthService.load().imgUrl;
   }
 }
